199daca4fc712c3584622c9722f74b56325eee73
svang56 <svang56@mail.csuchico.edu>
2018-02-07 17:50:57 -0800

    Character ASCII Value Defining Program

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main()
     else
     {
   //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
+  cout<<"$?! Pssh. What are you talking about?\n";
     }
 
   //no matter what they enter...
