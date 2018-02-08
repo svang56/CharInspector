2902d659af6d6b236bfdb053f749e857aef40a79
svang56 <svang56@mail.csuchico.edu>
2018-02-07 17:45:00 -0800

    Character ASCII Value Defining Program

--- main.cpp
+++ main.cpp
@@ -7,18 +7,29 @@ int main()
 {
 
   cout<<"What character do you want to know about?\n";
+  char letter;
+  cin>> letter;
 
+    if( letter >= 'A' && letter <= 'Z')
+    {
   //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
+  cout<< letter << " is an upper case letter!\n";
+    }
 
+    else if(letter >= 'a' && letter <= 'z')
+    {
   //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
+  cout<< letter << " is a lower case letter!\n";
+    }
 
+    else
+    {
   //in all other cases...
   cout<<"?! Pssh. What are you talking about?\n";
-  
+    }
+
   //no matter what they enter...
-  cout<<"The ASCII value is: ";
+  cout<<"The ASCII value is: " << (int)letter << endl;
 
   return 0;
 }
